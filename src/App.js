41df704906e6022555567598ed620b0c70a0b6d6
vgrosado <Vgrosado04@gmail.com>
2023-08-25 10:29:16 -0400

    commiting booking card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import HomePage from './Pages/HomePage/HomePage';
+import Header from './Components/Header/Header';
+import ExplorePage from './Pages/ExplorePage/ExplorePage';
 
 function App() {
   return (
+    <main className='App'>
     <BrowserRouter>
+      <Header/>
     <Routes>
       <Route path='/' element={<HomePage/>}></Route>
+      <Route path='/Explore' element={<ExplorePage/>}></Route>
     </Routes>
     </BrowserRouter>
+    </main>
+    
   );
 }
 
