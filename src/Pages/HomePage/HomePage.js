d9db30860a5c34762d82086b54b2be7110d88934
vgrosado <Vgrosado04@gmail.com>
2023-08-25 10:28:50 -0400

    working on booking card

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,21 +1,41 @@
 import '../HomePage/HomePage.scss'
 import heroVid from '../../Assets/Video/AdventureoftheSeas2023CruiseShipTour.Mp4'
-import Header from '../../Components/Header/Header';
+
 
 function HomePage() {
 
     return (
        <section>
-        <Header/>
             <div className='hero'>
                 <video className='hero__video' autoPlay loop muted src={heroVid}></video>
             </div>
             <main className='booking'>
+                <div className='booking__card'>
                 <h1>Find A Cruise!</h1>
                     <div className='booking__details'>
-                        <div className='booking__destination'>Destination</div>
-                        <div className='booking__port'>Choose A Port</div>
+                    <label className='booking__label'>Destination</label>
+                        <select className='booking__destination'>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                        </select>
+                        <label className='booking__label'>Departure Port</label>
+                        <select className='booking__port'>
+                        <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                        </select>
+                        <label className='booking'></label>
                         <input className='booking__date' type="date" min="2023-08-1" max="2023-08-31" step="7" />
+                        </div>
                     </div>
             </main>
        </section>
